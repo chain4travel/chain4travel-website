b91f5217382d437355f7d543a183528fe90e52a8
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2022-04-11 18:56:25 +0200

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,47 @@
+const now = String(Date.now())
+const htmlmin = require('html-minifier')
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addWatchTarget('./styles/tailwind.config.js')
+  eleventyConfig.addWatchTarget('./styles/tailwind.css')
+
+  eleventyConfig.addPassthroughCopy({ './_tmp/style.css': './style.css' })
+
+  eleventyConfig.addPassthroughCopy({
+    './node_modules/alpinejs/dist/cdn.js': './js/alpine.js',
+  })
+
+  eleventyConfig.addPassthroughCopy('images')
+  
+  eleventyConfig.addPassthroughCopy('favicon.ico')
+  eleventyConfig.addPassthroughCopy('favicon-16x16.png')
+  eleventyConfig.addPassthroughCopy('favicon-32x32.png')
+  eleventyConfig.addPassthroughCopy('android-chrome-192x192.png')
+  eleventyConfig.addPassthroughCopy('android-chrome-512x512.png')
+  eleventyConfig.addPassthroughCopy('site.webmanifest')
+
+  eleventyConfig.addShortcode('version', function () {
+    return now
+  })
+
+  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+    if (
+      process.env.ELEVENTY_PRODUCTION &&
+      outputPath &&
+      outputPath.endsWith('.html')
+    ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+      return minified
+    }
+
+        return content
+    })
+  
+  return {
+    htmlTemplateEngine: "njk"
+  };
+};
---
+++ styles/tailwind.config.js
@@ -0,0 +1,28 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+    content: ['_site/**/*.html'],
+    safelist: [],
+    plugins: [
+        require('@tailwindcss/typography'),
+    ],
+    theme: {
+        colors: {
+            dark: "#141A18",
+            primary: "#FF8235",
+            secondary: "#30E8BF",
+            muted: "#9CA3AF",
+            green: colors.lime,
+            gray: colors.neutral,
+        },
+        container: {
+            padding: {
+                DEFAULT: '1rem',
+                sm: '2rem',
+                lg: '4rem',
+                xl: '4rem',
+                '2xl': '4rem',
+                },
+        }
+      },
+  }
