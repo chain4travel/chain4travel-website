d5548658aa36432f0b491c6c8da71804cb3e431a
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2022-05-19 16:50:51 +0200

    more content updates

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
             gray: colors.neutral,
         },
         container: {
+            center: true,
             padding: {
                 DEFAULT: '1rem',
                 sm: '2rem',
