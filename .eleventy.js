829151a46d6d7ba9a39b0b6da41e723fd1a15c4b
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2023-05-16 08:00:39 +0200

    removed old files and updated data-cache

--- .eleventy.js
+++ .eleventy.js
@@ -30,10 +30,9 @@ module.exports = function (eleventyConfig) {
 		}
 
 		let metadata = await Image(src, {
-			widths: [300, 600, 1200, 2000],
-			formats: ["avif", "png", "jpg"],
-      outputDir: "./_site/images/",
-      urlPath: "/images/"
+			widths: [300, 600, 1200],
+			formats: ["avif", "png"],
+      outputDir: "./_site/img/"
 		});
 
 		let lowsrc = metadata.png[0];
