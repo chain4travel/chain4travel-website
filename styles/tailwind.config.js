a161ec347e85777f901c49099c92f2f97f398415
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2023-08-30 09:55:52 +0200

    added countdown

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -16,6 +16,7 @@ module.exports = {
         },
         fontFamily: {
             'sans': ['Inter, system-ui, sans-serif'],
+            'mono': ['ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace']
         },
         colors: {
             dark: "#141A18",
